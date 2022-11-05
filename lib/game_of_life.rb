73880a4fe3026c76d0eb30c55b2e9649f9488844
David Luque <david.luque@flywire.com>
2022-11-05 18:11:57 +0100

    [GREEN] Check no one matrix dimension

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,7 +1,7 @@
 # lib/string_calculator.rb
 class GameOfLife
   class InvalidGenerationError < StandardError; end
-  def check_generation(generation:)
-
+  def check_generation(*rows)
+    raise InvalidGenerationError if rows.size == 1
   end
 end
