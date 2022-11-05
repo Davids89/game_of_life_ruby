cb1c9ad71b25e6a27862fcff6343e38a8a0bd636
David Luque <david.luque@flywire.com>
2022-11-05 18:16:15 +0100

    [RED] No life in edges

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,6 +1,7 @@
 # lib/string_calculator.rb
 class GameOfLife
   class InvalidGenerationError < StandardError; end
+  class LifeInEdges < StandardError; end
   def check_generation(*rows)
     raise InvalidGenerationError if rows.size == 1
   end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -9,8 +9,19 @@ describe GameOfLife do
         generation = [true, false]
 
         expect do
-          described_class.new.check_generation(generation: generation)
+          described_class.new.check_generation(generation)
         end.to raise_error(described_class::InvalidGenerationError)
+      end
+    end
+
+    context 'when there is life in the edge' do
+      it 'raise error' do
+        first_row = [false, true, false]
+        second_row = [true, false, false]
+
+        expect do
+          described_class.new.check_generation(first_row, second_row)
+        end.to raise_error(described_class::LifeInEdges)
       end
     end
   end
