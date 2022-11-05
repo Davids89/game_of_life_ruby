de02c6cbb94bf910eccd1c35d3384ebe641dee1e
David Luque <david.luque@flywire.com>
2022-11-05 18:18:18 +0100

    [GREEN] No life in edges

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -4,5 +4,9 @@ class GameOfLife
   class LifeInEdges < StandardError; end
   def check_generation(*rows)
     raise InvalidGenerationError if rows.size == 1
+
+    rows.each do |row|
+      raise LifeInEdges if row.first == true || row.last == true
+    end
   end
 end
