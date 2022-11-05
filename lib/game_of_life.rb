475bc9b52dee640a833b2b13eec5468291463b6b
David Luque <david.luque@flywire.com>
2022-11-05 18:05:44 +0100

    [RED] Check generation requisites

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,3 +1,7 @@
 # lib/string_calculator.rb
 class GameOfLife
+  class InvalidGenerationError < StandardError; end
+  def check_generation(generation:)
+
+  end
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -3,4 +3,15 @@
 require_relative "../lib/game_of_life"
 
 describe GameOfLife do
+  describe '.check_iteration' do
+    context 'when an invalid generation is passed' do
+      it 'raise error' do
+        generation = [true, false]
+
+        expect do
+          described_class.new.check_generation(generation: generation)
+        end.to raise_error(described_class::InvalidGenerationError)
+      end
+    end
+  end
 end
